d1e18a4bb8bda228c03cf2cb6d51d57a698d22c7
Morgan Gilbert <morgan.el.gilbert@gmail.com>
2022-06-20 18:58:58 -0400

    added coding discussed in class, working on randomizer. pulled randomizer from robo gladiator with let characters for all characters.

--- script.js
+++ script.js
@@ -3,7 +3,39 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+// var character = a
+// const random =
 
+// var banana = Array[randomNumber];
+// var randomNumber = function(length) {
+//   var value = Math.floor(math.random() * 21) + 40;
+// };
+function generatePassword() {
+  var length = prompt('How long?')
+  console.log(length);
+  if (length < 8 || length > 128) {
+      alert('Password must be between 8 and 128 characters.');
+      return generatePassword();
+  }
+  if (length >= 8 || length < 128) {
+    var confirmSpecial = window.confirm('Would you like to include special characters?');
+    if (confirmSpecial) {
+      // passwordText.value = ;
+      // passwordText.vaue = randomNumber;
+      let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()';
+      
+      let str = '';
+      for (let i = 0; i < length; i++) {
+        str += characters.charAt(Math.floor(Math.random() * characters.length));
+      }
+      return str;
+    }
+    else {
+      alert('Password does not include special characters.');
+    }
+  }
+  return "password";
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
