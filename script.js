7482535b7e3c76da988174ee872d3d571bb69779
Morgan Gilbert <morgan.el.gilbert@gmail.com>
2022-06-21 22:11:17 -0400

    updated text verbiage

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ var generateBtn = document.querySelector("#generate");
 
 // generatePassword function
 function generatePassword() {
-  var length = prompt('How long?')
-  console.log(length);
+  var length = prompt('How long would you like the password? Please choose a number of at least 8 and less than 128.')
+  // console.log(length);
 
   // If the length is less than 8 or greater than 128, will return to ask How long?
   if (length < 8 || length > 128) {
@@ -14,13 +14,13 @@ function generatePassword() {
 
   // Adding criteria for true/false statements and outcomes
   if (length >= 8 || length < 128) {
-    // Variables for special, number, uppercase, and lowercase characters
+    // Variables for special, number, uppercase, and lowercase character criteria
     var confirmSpecial = window.confirm('Would you like to include special characters?');
     var confirmNumbers = window.confirm('Would you like to include numbers?');
     var confirmUpper = window.confirm(' Would you like to include uppercase letters?');
     var confirmLower = window.confirm('Would you like to include lowercase letters?');
 
-    // Outcomes depending on True/False scenerios for character selections
+    // Outcomes depending on True/False scenerios for character criteria selections
     if (confirmSpecial && confirmNumbers && confirmUpper && confirmLower) {
       return selectPassword('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()', length); 
     }
